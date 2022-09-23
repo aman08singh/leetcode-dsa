5a98356b56debab2ab364140e5988408c3aeef15
aman08singh <aman.rajkumar.singh@gmail.com>
2022-09-23 18:02:04 +0000

    [LeetCode Sync] Runtime - 82 ms (2.89%), Memory - 44.1 MB (100.00%)

---
+++ my-folder/0258-add-digits/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    if(num < 10) {
+        return num;
+    }
+    let sum=0;
+    while(num>0) {
+        sum=sum+num%10;
+        num=parseInt(num/10);
+    }
+    return addDigits(parseInt(sum));
+};
