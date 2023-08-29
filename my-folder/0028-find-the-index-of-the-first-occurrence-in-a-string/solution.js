b5e7830b0de71eb7cf3284df8fd0ecc37eb46441
aman08singh <aman.rajkumar.singh@gmail.com>
2023-08-29 05:16:54 +0000

    [LeetCode Sync] Runtime - 52 ms (14.45%), Memory - 43 MB (100.00%)

--- my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.js
+++ my-folder/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.js
@@ -4,8 +4,35 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if(!haystack) {
-        return -1;
+    if (!haystack || !needle) {
+    return 0;
+  } else if (haystack === needle) {
+    return 0;
+  } else {
+    let firstIndex=-1, matchingNeedle = '';;
+    for(let i=0;i<haystack.length;i++) {
+      if(haystack.length - i >= needle.length) {
+        for(let j=0;j<needle.length;j++) {
+          if(haystack[i+j] === needle[j]) {
+            if(firstIndex < 0) {
+              firstIndex = i;
+            }
+            matchingNeedle+=needle[j];
+            if(matchingNeedle === needle) {
+              return firstIndex;
+            }
+          }
+          else {
+            firstIndex = -1;
+            matchingNeedle='';
+            break;
+          }
+        }
+      }
+      else {
+      break;
+      }
     }
-    return haystack.indexOf(needle);
+    return -1;
+  }
 };
