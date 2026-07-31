6565ac38e4ffb680a9c5411fa1162848be187a78
aman08singh <aman.rajkumar.singh@gmail.com>
2026-07-31 19:18:30 +0000

    [LeetCode Sync] Runtime - 31 ms (38.22%), Memory - 53.4 MB (96.47%)

---
+++ my-folder/0001-two-sum/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let index = [];
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i + 1; j < nums.length; j++) {
+      sum = nums[i] + nums[j];
+      if (sum === target) {
+        index.push(i);
+        index.push(j);
+        break;
+      }
+    }
+    if (sum === target) break;
+  }
+  return index;
+};
