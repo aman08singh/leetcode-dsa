9280eea0cd75bfaaadd1a7c1a04801bcf313f08a
aman08singh <aman.rajkumar.singh@gmail.com>
2026-08-07 17:29:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 53.8 MB (46.21%)

---
+++ my-folder/0027-remove-element/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    if(!nums || nums.length < 1) return 0;
+    else if(val === 0 && !nums.includes(val)) return nums.length;
+    for(let i=0;i<nums.length;i++) {
+        if(nums[i] === val){
+            nums.splice(i, 1)[0];
+            i--;
+        }
+    }
+    return nums.length;
+};
+// console.log("Answer ", removeElement([4,4,0,1,0,2], 0));
