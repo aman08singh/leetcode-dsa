344ce153525958d5bd2201d04f2125618bcf73ea
aman08singh <aman.rajkumar.singh@gmail.com>
2026-08-07 15:43:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 56.6 MB (89.05%)

---
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    // const uniqueSet = new Set();
+    if(!nums || nums?.length < 1) return 0;
+    // nums?.forEach((e, i) => {
+    //     if()
+    // })
+    let writeIndex = 1;
+    for(let i=1;i<nums.length;i++){
+        if(nums[i]!==nums[i-1]) {
+            nums[writeIndex] = nums[i];
+            writeIndex = writeIndex + 1;
+        }
+    }
+    return writeIndex;
+};
+// console.log("Answer is ", removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
