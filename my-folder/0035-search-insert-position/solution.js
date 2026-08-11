16b3c05da31b03991b1e34cdc0031482dadef470
aman08singh <aman.rajkumar.singh@gmail.com>
2026-08-11 17:29:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 53.8 MB (45.99%)

---
+++ my-folder/0035-search-insert-position/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  if (!nums || nums.length < 1) return 0;
+  let i = 0,
+    index;
+  while (i < nums.length) {
+    if (nums[i] === target) {
+      index = i;
+      break;
+    } else if (target < nums[i]) {
+      index = i;
+      break;
+    }
+    i = i + 1;
+  }
+  if (index === undefined) return nums.length;
+  return index;
+};
+// console.log("Answer is ", searchInsert([1,3,5,6], 2));
